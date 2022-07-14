afe7e9e2fa69e577e696813e7c6bfae77c73f01a
Segun Abisagbo <segebee@gmail.com>
2022-07-14 11:15:23 +0100

    Aadd web3modal and etherjs to test connecting user wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from 'ethers';
+import { useCallback } from 'react';
+import Web3Modal from 'web3modal';
+
+
 
 function App() {
+
+  const onConnectWallet = useCallback(
+    async () => {
+      const walletConnectModal = new Web3Modal({
+        network: "mainnet",
+      });
+      await walletConnectModal.connect();
+      console.log('hello')
+    },
+    [],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h2>Welcome to On Drip</h2>
       </header>
+
+      <button onClick={onConnectWallet}>Connect Wallet</button>
     </div>
   );
 }
