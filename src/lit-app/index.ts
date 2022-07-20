ba91b2a477bbf017501f542488913c402ae87c0d
thisisommore <ommore501@gmail.com>
2022-07-20 16:59:06 +0000

    lit: Use proper encoding and decoding

--- src/lit-app/index.ts
+++ src/lit-app/index.ts
@@ -3,9 +3,9 @@ import LitJsSdk from "lit-js-sdk";
 import { OnDripMarketPlace__factory, OnDripNFT__factory } from "../contracts";
 import { getAccessControlConditions } from "./access-control";
 import { chain } from "./config"
-type LitEncryptedPayload = {
-    excryptedKey: string,
-    excrypedtedData: string
+type SmartContractCreds = {
+    excryptedKey: string, //hex
+    excrypedtedData: string //hex
 }
 
 export async function litEncrypt(tokenId: number, username: string, password: string): Promise<string> {
@@ -22,17 +22,20 @@ export async function litEncrypt(tokenId: number, username: string, password: st
             password,
         })
     );
-
+    let encryptedStringAsArrayBuffer = await (encryptedString as Blob).arrayBuffer()
+    let encryptedStringAsHex = Buffer.from(encryptedStringAsArrayBuffer).toString("hex")
     const encryptedSymmetricKey = await litClient.saveEncryptionKey({
         accessControlConditions,
         symmetricKey,
         authSig,
-        chain: "ethereum",
+        chain,
     });
 
-    const litEncryptedPayload: LitEncryptedPayload = {
-        excrypedtedData: encryptedString,
-        excryptedKey: encryptedSymmetricKey
+    const encryptedSymmetricKeyHex = Buffer.from(encryptedSymmetricKey).toString("hex")
+
+    const litEncryptedPayload: SmartContractCreds = {
+        excrypedtedData: encryptedStringAsHex,
+        excryptedKey: encryptedSymmetricKeyHex
     }
 
     return JSON.stringify(litEncryptedPayload)
