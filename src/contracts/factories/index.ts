94e0c76c58ac4d20292342f466644c250bb37e44
Falco <alecmichel@gmail.com>
2022-08-19 02:51:19 -0700

    factory added for market

--- src/contracts/factories/index.ts
+++ src/contracts/factories/index.ts
@@ -2,6 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export { OnDripDirectLicense__factory } from "./OnDripDirectLicense__factory";
+export { OnDripMarket__factory } from "./OnDripMarket__factory";
 export { OnDripMarketPlace__factory } from "./OnDripMarketPlace__factory";
 export { OnDripNFT__factory } from "./OnDripNFT__factory";
 export { OnDripSaas__factory } from "./OnDripSaas__factory";
