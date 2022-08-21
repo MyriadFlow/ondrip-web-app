5b90bd9ac7e73bab5ea5bbbd82e15fe2ce69b1c5
thisisommore <ommore501@gmail.com>
2022-08-21 13:52:52 +0000

    Introduce direct license collection creation

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,7 @@ export const infuraId = "504b80c00c534f598bb3f254e4b9db74";
 export const graphQLUri = "https://api.thegraph.com/subgraphs/name/thisisommore/ondrip-subgraph";
 export const nftContractAddress = "0x151A0bbDC9926Fbe1Ba63d11EbC62E5eBd58Fc57";
 export const nftMarketPlaceContractAddress = "0x3aEF3cA504496e000Fe2aa25F10b4aE34Decb04c";
+export const onDripDl = "0x3971D551157D5bFE66d31b1Cf444d0c4c4b693E6";
 export const NFT_STORAGE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEQ0QmI1ZjkyMGM4NDkxNEM3N2IyYzczMTcyRThCMzAwOTA3MDk4NDAiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1ODY1ODE0OTQ5NCwibmFtZSI6ImhhY2tmcyJ9.AGGEVH7cS7OMYZZYnR81SGf30anuTe4bmhoSEHr5UFI';
 export const services = [
     {
--- src/pages/DirectLicense.tsx
+++ src/pages/DirectLicense.tsx
@@ -1,22 +1,38 @@
 import {
-    Flex,
-    Box,
-    Heading,
-    Button,
-    useDisclosure,
-  } from "@chakra-ui/react";
-  import CreateDirectLicenseModal from "../components/Modal/CreateDirectLicenseModal";
+  Flex,
+  Box,
+  Heading,
+  Button,
+  useDisclosure,
+  Grid,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Card from "../components/Card";
+import CreateDirectLicenseModal from "../components/Modal/CreateDirectLicenseModal";
 
+type tCollection = {
+  name: string;
+  symbol: string;
+  royaltyFeeBips: number;
+};
 function DirectLicense() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [collections, setCollections] = useState<tCollection[]>([]);
+
+  useEffect(() => {
+    const _collections = JSON.parse(
+      localStorage.getItem("dlCollections") ?? "[]"
+    ) as tCollection[];
+    setCollections(_collections);
+  }, []);
   return (
     <Box py={5} className="profile">
-         <Flex className="user-nfts" flexDirection="column">
+      <Flex className="user-nfts" flexDirection="column">
         <Flex my={5} alignItems="center">
-        <Heading fontSize="2xl" my={5}>
-        Direct License
-        </Heading>
-        <Button
+          <Heading fontSize="2xl" my={5}>
+            Direct License
+          </Heading>
+          <Button
             onClick={onOpen}
             backgroundColor="#266011"
             color="white"
@@ -26,9 +42,27 @@ function DirectLicense() {
           >
             Add
           </Button>
-          </Flex>
-          </Flex>
-          {isOpen && <CreateDirectLicenseModal isOpen={isOpen} onClose={onClose} />}
+        </Flex>
+        <Grid
+          className="card-list"
+          my={3}
+          templateColumns="repeat(4, 1fr)"
+          gap={3}
+        >
+          {collections.map((e, i) => (
+            <Card
+              key={i}
+              primaryLabel={e.name}
+              secondaryLabel={e.symbol}
+              primaryButtonLabel="Mint"
+              primaryButtonAction={() => console.log("Mint")}
+              secondaryButtonLabel={e.royaltyFeeBips.toString() + " Royalty"}
+              isUser
+            />
+          ))}
+        </Grid>
+      </Flex>
+      {isOpen && <CreateDirectLicenseModal isOpen={isOpen} onClose={onClose} />}
     </Box>
   );
 }
